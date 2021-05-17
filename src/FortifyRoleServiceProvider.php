2df80de790a040211ad4bc54533df753d2bf4988
Benny Rahmat <akunbeben@gmail.com>
2021-05-18 04:57:32 +0800

    refactor: using auth() helper instead of read through database in the middleware

--- src/FortifyRoleServiceProvider.php
+++ src/FortifyRoleServiceProvider.php
@@ -21,11 +21,6 @@ class FortifyRoleServiceProvider extends ServiceProvider
             'role', 
             CheckRoles::class
         );
-
-        $this->app->singleton(
-            \Laravel\Fortify\Contracts\LoginResponse::class,
-            \App\Http\Responses\LoginResponse::class
-        );
     }
 
     /**
@@ -51,6 +46,7 @@ class FortifyRoleServiceProvider extends ServiceProvider
                 __DIR__.'/Http/Middleware/RedirectIfAuthenticated.php' => app_path('Http/Middleware/RedirectIfAuthenticated.php'),
                 __DIR__.'/Http/Responses/LoginResponse.php' => app_path('Http/Responses/LoginResponse.php'),
                 __DIR__.'/Models/Role.php' => app_path('Models/Role.php'),
+                __DIR__.'/Providers/FortifyServiceProvider.php' => app_path('Providers/FortifyServiceProvider.php'),
             ], 'fortify-role-support');
 
             $this->publishes([
